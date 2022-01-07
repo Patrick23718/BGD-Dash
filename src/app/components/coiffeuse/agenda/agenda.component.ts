e1a002c77184b5807a126d84f45b6da3a185613f
Patrick23718 <noukimi.patrick@gmail.com>
2022-01-07 17:08:52 +0100

    init

--- src/app/components/coiffeuse/agenda/agenda.component.ts
+++ src/app/components/coiffeuse/agenda/agenda.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-agenda',
   templateUrl: './agenda.component.html',
-  styleUrls: ['./agenda.component.scss']
+  styleUrls: ['./agenda.component.scss'],
 })
 export class AgendaComponent implements OnInit {
+  headers = ['Nom&prénom', 'Mail', 'Téléphone', "Nombre d'avis", 'Action'];
+  reservations = [
+    {
+      img: 'assets/profil.png',
+      nom: 'Lille',
+      mail: '2021-04-10',
+      tel: '0123456789',
+      avis: 4,
+    },
+    {
+      img: 'assets/profil.png',
+      nom: 'Lille',
+      mail: '2021-04-10',
+      tel: '0123456789',
+      avis: 4,
+    },
+    {
+      img: 'assets/profil.png',
+      nom: 'Lille',
+      mail: '2021-04-10',
+      tel: '0123456789',
+      avis: 4,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
