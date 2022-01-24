18f51d52a22f06e110660376222cb9a9a1642035
Patrick23718 <noukimi.patrick@gmail.com>
2022-01-24 08:05:40 +0100

    test

--- src/app/pages/contact/cliente-coiffeuse/details-cliente-coiffeuse/details-cliente-coiffeuse.component.ts
+++ src/app/pages/contact/cliente-coiffeuse/details-cliente-coiffeuse/details-cliente-coiffeuse.component.ts
@@ -14,56 +14,68 @@ export class DetailsClienteCoiffeuseComponent implements OnInit {
 
   users: any = [];
   cid = '';
+
+  allmessages: any[] = [];
   constructor(private router: ActivatedRoute, private socket: Socket) {
     this.router.params.subscribe((paramsId) => {
       this.cid = paramsId.cid;
       console.log(this.cid);
+      this.getUsers().subscribe((res: any) => {
+        this.allmessages = res;
+        this.teste(res);
+      });
     });
   }
 
   ngOnInit(): void {
-    this.getUsers().subscribe((res: any) => {
-      const array = res.map((elt: any) => {
-        var id;
-        if (elt.toId._id === this.cid) {
-          id = {
-            _id: elt._id,
-            coiffeuse: elt.toId,
-            cliente: elt.fromId,
-            createdAt: elt.createdAt,
-            imageURL: elt.imageURL,
-            isRead: elt.isRead,
-            message: elt.message,
-          };
-        } else if (elt.fromId._id === this.cid) {
-          id = {
-            _id: elt._id,
-            cliente: elt.toId,
-            coiffeuse: elt.fromId,
-            createdAt: elt.createdAt,
-            imageURL: elt.imageURL,
-            isRead: elt.isRead,
-            message: elt.message,
-          };
-        }
-        return id;
-      });
-      console.log(array);
+    // this.getUsers().subscribe((res: any) => {
+    // });
+  }
 
-      for (var i = 0; i < array.length; i++) {
-        var test = false;
-        for (var j = 0; j < this.users.length; j++) {
-          if (this.users[j]?.cliente._id === array[i]?.cliente?._id) {
-            test = true;
-            break;
-          }
-        }
-        if (!test) {
-          this.users.push(array[i]);
-          console.log(this.users);
+  teste(teste: any) {
+    this.allmessages = teste;
+    console.log(this.allmessages);
+    const array = this.allmessages.reverse().map((elt: any) => {
+      var id;
+      if (elt.toId._id === this.cid) {
+        id = {
+          _id: elt._id,
+          coiffeuse: elt.toId,
+          cliente: elt.fromId,
+          createdAt: elt.createdAt,
+          imageURL: elt.imageURL,
+          isRead: elt.isRead,
+          message: elt.message,
+        };
+      } else if (elt.fromId._id === this.cid) {
+        id = {
+          _id: elt._id,
+          cliente: elt.toId,
+          coiffeuse: elt.fromId,
+          createdAt: elt.createdAt,
+          imageURL: elt.imageURL,
+          isRead: elt.isRead,
+          message: elt.message,
+        };
+      }
+      return id;
+    });
+    console.log(array);
+
+    for (var i = 0; i < array.length; i++) {
+      var test = false;
+      for (var j = 0; j < this.users.length; j++) {
+        if (this.users[j]?.cliente._id === array[i]?.cliente?._id) {
+          test = true;
+          break;
         }
       }
-    });
+      if (!test) {
+        this.users.push(array[i]);
+        console.log(this.users);
+      }
+    }
+    this.allmessages = this.allmessages.reverse();
   }
 
   getUsers() {
@@ -71,7 +83,8 @@ export class DetailsClienteCoiffeuseComponent implements OnInit {
     this.socket.emit('get-message', {});
     const observable = new Observable((observer) => {
       this.socket.on('getmessage', (data: any) => {
-        // console.log(data);
+        console.log(data);
+        // this.allmessages = data;
         observer.next(data);
       });
     });
@@ -79,36 +92,39 @@ export class DetailsClienteCoiffeuseComponent implements OnInit {
   }
 
   getAllMessage(clienteid: string) {
-    this.getUsers().subscribe((res: any) => {
-      this.messages = res.map((elt: any) => {
-        var id;
-        if (elt.toId._id === this.cid && elt.fromId._id === clienteid) {
-          id = elt;
-          // {
-          //   _id: elt._id,
-          //   coiffeuse: elt.toId,
-          //   cliente: elt.fromId,
-          //   createdAt: elt.createdAt,
-          //   imageURL: elt.imageURL,
-          //   isRead: elt.isRead,
-          //   message: elt.message,
-          //   sender:
-          // };
-        } else if (elt.fromId._id === this.cid && elt.toId._id === clienteid) {
-          id = elt;
-          // {
-          //   _id: elt._id,
-          //   cliente: elt.toId,
-          //   coiffeuse: elt.fromId,
-          //   createdAt: elt.createdAt,
-          //   imageURL: elt.imageURL,
-          //   isRead: elt.isRead,
-          //   message: elt.message,
-          // };
-        }
-        return id;
-      });
-      console.log(this.messages);
+    var test = [];
+    for (var i = this.allmessages.length - 1; (i = 0); i++) {
+      test.push(this.allmessages[i]);
+    }
+
+    this.messages = this.allmessages.map((elt: any) => {
+      var id;
+      if (elt.toId._id === this.cid && elt.fromId._id === clienteid) {
+        id = elt;
+        // {
+        //   _id: elt._id,
+        //   coiffeuse: elt.toId,
+        //   cliente: elt.fromId,
+        //   createdAt: elt.createdAt,
+        //   imageURL: elt.imageURL,
+        //   isRead: elt.isRead,
+        //   message: elt.message,
+        //   sender:
+        // };
+      } else if (elt.fromId._id === this.cid && elt.toId._id === clienteid) {
+        id = elt;
+        // {
+        //   _id: elt._id,
+        //   cliente: elt.toId,
+        //   coiffeuse: elt.fromId,
+        //   createdAt: elt.createdAt,
+        //   imageURL: elt.imageURL,
+        //   isRead: elt.isRead,
+        //   message: elt.message,
+        // };
+      }
+      return id;
     });
+    console.log(this.messages);
   }
 }
